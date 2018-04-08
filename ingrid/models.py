ce527f717a16b7d67c0b978abc070be309282a0d
Tomek <tomaszmf95@yahoo.com>
2018-04-08 14:31:42 -0700

    Made sizes tuple

--- ingrid/models.py
+++ ingrid/models.py
@@ -8,11 +8,12 @@ class Wetsuit(models.Model):
     genders = (("M", "Mens"),
                ("W", "Womens"),
                ("U", "Unisex"))
-    sizes = ("XL", "L", "M", "S", "XS")
+    sizes = (("XXL", "XX Large"), ("XL", "Extra Large"), ("L", "Large"), ("M", "Medium"), ("S", "Small"))
+    # TODO: include women's sizes, (even numbers 2-14)
 
     rfid = models.CharField(max_length=10, unique=True)
-    gender = models.CharField(choices=genders)
-    size = models.CharField(choices=sizes)
+    gender = models.CharField(choices=genders, max_length=1)
+    size = models.CharField(choices=sizes, max_length=4)
 
     def __str__(self):
         return "{} {} wetsuit".format(self.size, self.gender)
@@ -20,7 +21,7 @@ class Wetsuit(models.Model):
 
 class Locker(models.Model):
 
-    locker_id = models.CharField(primary_key=True)
+    locker_id = models.CharField(primary_key=True, max_length=5)
     wetsuit = models.ForeignKey(to=Wetsuit, on_delete=models.CASCADE)
     has_suit = models.BooleanField(default=True)
 
