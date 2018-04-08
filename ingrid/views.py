74c855915ea541ba7eca5a5b4bf5ebac2adce654
Tomek <tomaszmf95@yahoo.com>
2018-04-08 14:20:33 -0700

    Dynamically modify success url based on form response

--- ingrid/views.py
+++ ingrid/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, FormView
+
+from .forms import IndexForm
+from .convinience import is_member_rfid, is_wetsuit_rfid
+
 
 # Create your views here.
+class IndexView(FormView):
+
+    template_name = 'index.html'
+    form_class = IndexForm
+
+    def get_success_url(self):
+        form = self.get_form()
+        rfid = form.cleaned_data['rfid']
+
+        if is_wetsuit_rfid(rfid):
+            return '/return/'
+        elif is_member_rfid(rfid):
+            return '/checkout/'
+        else:
+            return ''
+
+
+class WetsuitListView(ListView):
+    pass
