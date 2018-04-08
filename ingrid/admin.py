6f31b59dda52cb6a38fa5ad852c5d1479a69ddfa
Tomek <tomaszmf95@yahoo.com>
2018-04-08 16:08:53 -0700

    Fixed model registration

--- ingrid/admin.py
+++ ingrid/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Wetsuit, Locker
 
 # Register your models here.
-admin.register(Wetsuit)
-admin.register(Locker)
+admin.site.register(Wetsuit)
+admin.site.register(Locker)
