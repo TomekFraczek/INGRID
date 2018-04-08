3a7d38e967b38f592460a2782b8f77a04a76a170
Tomek <tomaszmf95@yahoo.com>
2018-04-08 15:24:20 -0700

    added initial autofucus on rfid field

--- ingrid/forms.py
+++ ingrid/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class IndexForm(forms.Form):
 
-    rfid = forms.CharField()#widget=forms.HiddenInput)
+    rfid = forms.CharField(widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))#widget=forms.HiddenInput)
 
     def send_email(self):
         # send email using the self.cleaned_data dictionary
