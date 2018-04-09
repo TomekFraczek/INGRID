9218e78680b3151c5e1e075cdc203c3ff0aece89
Tomek <tomaszmf95@yahoo.com>
2018-04-08 17:22:43 -0700

    Sucessfully added reference to image

--- ingrid/urls.py
+++ ingrid/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('checkout/', views.LockerListView.as_view(), name='checkout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
