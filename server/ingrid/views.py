12f495806885cafe05cb709e73488fdac92c87ab
Tomek <tomaszmf95@yahoo.com>
2018-04-11 22:51:03 -0700

    added view to ensure a healthy system state before returning to index

--- server/ingrid/views.py
+++ server/ingrid/views.py
@@ -1,14 +1,14 @@
 from django.urls import reverse
-from django.views.generic import ListView, FormView, DetailView
+from django.views.generic import ListView, FormView, DetailView, RedirectView
 
 from .forms import IndexForm
 from .models import Locker
-# from .control import doors_open
+from .control import doors_open
 from .convinience import is_member_rfid, is_wetsuit_rfid
 
 
-# Create your views here.
 class IndexView(FormView):
+    """The homepage view that waits for someone to scan a RFID tags"""
 
     template_name = 'index.html'
     form_class = IndexForm
@@ -27,38 +27,56 @@ class IndexView(FormView):
 
 
 class LockerListView(ListView):
+    """View that displays all the wetsuits available for checkout"""
 
     template_name = 'checkout.html'
     model = Locker
 
 
 class DispenseView(DetailView):
+    """View that handles the giving out of a wetsuit"""
 
     model = Locker
     pk_url_kwarg = 'locker_id'
     template_name = 'dispense.html'
 
     def get(self, request, *args, **kwargs):
-        # Locker.lock.open()
+        Locker.should_have_suit = False
+        Locker.lock.open()
         return super(DispenseView, self).get(request, *args, **kwargs)
 
 
 class ReturnView(DetailView):
+    """View that handles the returning of a wetsuit"""
 
     model = Locker
     pk_url_kwarg = 'locker_id'
     template_name = 'return.html'
 
 
-class CloseDoorView(DetailView):
+class CloseDoorView(RedirectView):
+    """View called upon the closing of all doors that ensures a healthy state"""
 
-    model = Locker
     pk_url_kwarg = 'locker_id'
     template_name = 'return.html'
 
-    def dispatch(self, request, *args, **kwargs):
-        # doors_open.wait_for_inactive()  # According to docs should be .wait_for_release() ?
-        return super(CloseDoorView, self).dispatch(request, *args, **kwargs)
+    def get_redirect_url(self, *args, **kwargs):
+
+        redirect_url = reverse('index')
+
+        for locker in Locker.objects.all():
+
+            # If a locker is missing a suit, demand it back
+            if locker.should_have_suit and not locker.has_suit:
+                redirect_url = reverse('return', kwargs={'locker_id': locker.locker_id})
+
+            # If a locker unexpectedly has a suit, set the expected status to have_suit
+            if not locker.should_have_suit and locker.has_suit:
+                locker.should_have_suit = True
+
+        return redirect_url
+
+
 
 
 
