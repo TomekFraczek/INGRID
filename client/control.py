be650473d82ad14e25465a594a373acdd4f17966
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-04-14 15:10:19 -0700

    Lint files with Pycharm

--- client/control.py
+++ client/control.py
@@ -7,8 +7,5 @@ settings_file = open(os.path.join(os.getcwd(), 'config.json'))
 settings = load(settings_file)
 gpio_settings = settings['GPIO Pins']
 
-
 #: sensor that detects if any of the doors are open
 doors_open = Button(gpio_settings['door open pin'])
-
-
