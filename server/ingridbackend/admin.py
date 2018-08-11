08e13130570fb53722e324cdf62b377a6b6526f8
TomekFraczek <tomaszmf95@yahoo.com>
2018-08-10 19:01:53 -0700

    Deleted old django project

--- server/ingridbackend/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Wetsuit, Locker
-
-# Register your models here.
-admin.site.register(Wetsuit)
-admin.site.register(Locker)
--- server/ingridbackend/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class IndexForm(forms.Form):
-    rfid = forms.CharField(widget=forms.TextInput(attrs={'autofocus': 'autofocus'}))  # widget=forms.HiddenInput)
-
-    def send_email(self):
-        # send email using the self.cleaned_data dictionary
-        pass
